31337e91232689da9fb9c268dc3f46cb2af1a8f4
jcg01423690 <jcg01423690@alibaba-inc.com>
2022-11-13 15:22:50 +0800

    使用webpack打包

--- src/index.ts
+++ src/index.ts
@@ -1,100 +1,43 @@
 import { IPluginContext } from '@tarojs/service';
-import { readConfig } from "@tarojs/helper";
-import * as path from 'path'
-import fs from 'fs';
+// import { readConfig } from "@tarojs/helper";
+import rollup, {OutputOptions} from 'rollup';
+import jsx from 'rollup-plugin-jsx'
+import less from "rollup-plugin-less";
+
+const inputOptions = {
+  input: 'src/components/Button/index.tsx',
+  external: id => /react|@tarojs/.test(id),
+  plugins: [
+    jsx( {factory: 'React.createElement'} ),
+    less({
+      insert: true,
+      output: 'es/Button/index.css'
+    }),
+  ],
+};
+const outputOptions: OutputOptions = {
+  dir: 'es',
+  format: 'esm'
+};
+
+async function build () {
+  const bundle = await rollup.rollup(inputOptions);
+
+  const { output } = await bundle.generate(outputOptions);
+  console.log('=======output===========', output);
+
+  await bundle.write(outputOptions);
+}
 
 export default (ctx: IPluginContext) => {
-  const {paths, platforms, helper, runOpts} = ctx
-  const {appPath, outputPath, sourcePath, configPath} = paths
-  // const appConfigFilePath = resolveScriptPath(path.join(sourcePath, `${helper.ENTRY}.config`))
-  // const appConfig = helper.readConfig(appConfigFilePath)
-  console.log('=======ctx===========', ctx)
-  if (ctx.platforms.has('h5')) {
-    ctx.modifyWebpackChain((args) => {
-      const chain = args.chain;
-      // console.log('=======before modify===========', chain.toConfig());
-      modifyEntry(chain);
-      // modifyMode(chain);
-      modifyOutput(chain)
-      modifyPerformance(chain)
-      modifyPlugins(chain);
-      removeResolve(chain);
-      modifyModule(chain);
-      removeOptimization(chain);
-      modifyExternals(chain);
-      console.log('=======after modify===========', chain.toConfig());
-    })
-
-  }
+  ctx.registerPlatform({
+    name: 'h5-native-component',
+    useConfigName: 'h5',
+    async fn({config}) {
+      console.log('=======dddd===========', __dirname, process.cwd());
+      // todo 如何才能调用tsc进行编译呢？tsc不能对一个特定的文件夹使用另外的tsconfig.json来编译，同时taro项目可能是使用ts编写的，那么我只能使用命令行参数的方式了？
+      // build();
+    }
+  })
 }
 
-function modifyEntry(chain) {
-  // 删除旧配置
-  chain.entryPoints.clear();
-  // 需要拿到app.config.json，找到components
-  const appConfigJsFile = path.join(process.cwd(), './src', 'app.config.js');
-  const appConfigTsFile = path.join(process.cwd(), './src', 'app.config.ts');
-  const appConfig = fs.existsSync(appConfigJsFile) ? appConfigJsFile
-    : fs.existsSync(appConfigTsFile) ? appConfigTsFile : null;
-  if (appConfig) {
-    const config = readConfig(appConfig)
-    const components = config.components;
-    components.forEach(compPath => {
-      const fullPath = `${compPath}.tsx`; // 添加文件格式
-      const dirs = fullPath.split('/');
-      const compName = dirs[dirs.length - 2]; // 取文件夹名作为组件名，最后一个是index.jsx
-      chain.entry(compName).add(`./src/${fullPath}`).end();
-      console.log('=======xxx===========', compName, `./src/${fullPath}`);
-    })
-    // 使用components数据拼接entry对象
-  } else {
-    console.log('=======nnnnnn===========', );
-  }
-}
-
-function modifyMode(chain) {
-  chain.mode('development');
-}
-
-function modifyOutput(chain) {
-  chain.output.filename('[name]/index.js');
-  chain.output.libraryTarget('umd');
-  chain.output.globalObject('this');
-  chain.output.libraryExport('default');
-}
-
-
-function modifyPerformance(chain) {
-  // 删除旧配置
-  chain.performance.clear();
-}
-
-function modifyPlugins(chain) {
-  chain.plugins.delete('miniPlugin');
-  chain.plugins.delete('miniSplitChunksPlugin');
-  chain.plugins.delete('definePlugin');
-
-}
-
-function removeResolve(chain) {
-  chain.resolve.alias.clear();
-  chain.resolve.plugins.clear();
-  chain.resolve.mainFields.clear();
-  chain.resolve.extensions.clear();
-}
-function modifyModule(chain) {
-  chain.module.rules.clear();
-  chain.module.rule('babel').test(/\.(ts|tsx)$/).use('ts-loader').loader('ts-loader')
-}
-
-function removeOptimization(chain) {
-  // chain.optimization.minimizers.clear();
-  chain.optimization.minimize(false);
-  chain.optimization.usedExports(false);
-
-}
-
-function modifyExternals(chain) {
-  chain.externals(/^(@tarojs|react)/)
-  // chain.externals(/^react/)
-}
