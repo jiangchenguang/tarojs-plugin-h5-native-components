960b4b8647647aeed3353ee3623a5d783ef1788d
jcg01423690 <jcg01423690@alibaba-inc.com>
2022-12-04 09:55:21 +0800

    修正测试异步报错

--- test/compileOneComponent.test.js
+++ test/compileOneComponent.test.js
@@ -3,26 +3,32 @@ import compile, { compileOneComponent } from '../src/compile.js';
 
 
 describe('compileOneComponent', function() {
-  it('编译最简单的tsx组件', async function() {
-    // const { output } = await compileOneComponent(path.resolve(__dirname, './component/simpleComponent/simpleComponent.tsx'), '/component/');
-    await compile(path.resolve(__dirname, './component/simpleComponent/simpleComponent.tsx'), 'component');
+  it('编译最简单的tsx组件', function() {
+    return new Promise( async (resolve, reject) => {
+      await compile(path.resolve(__dirname, './component/simpleComponent/simpleComponent.tsx'), 'component');
+      resolve();
+    })
   });
-  it('编译最简单的jsx组件', async function() {
-    // const { output } = await compileOneComponent(path.resolve(__dirname, './component/simpleComponent-1/simpleComponent.jsx'));
-    await compile(path.resolve(__dirname, './component/simpleComponent-1/simpleComponent.jsx'), 'component');
+  it('编译最简单的jsx组件', function() {
+    return new Promise(async resolve => {
+      await compile(path.resolve(__dirname, './component/simpleComponent-1/simpleComponent.jsx'), 'component');
+      resolve();
+    })
   });
   xit('可以编译css')
   xit('可以编译less')
   xit('可以编译scss')
-  it('编译带子组件的tsx组件', async function() {
-    // const { output } = await compileOneComponent(
-    //   path.resolve(__dirname, './component/useChildComponent/useChildComponent.tsx'),
-    //   );
-    await compile(path.resolve(__dirname, './component/useChildComponent/useChildComponent.tsx'), 'component');
+  it('编译带子组件的tsx组件', function() {
+    return new Promise(async resolve => {
+      await compile(path.resolve(__dirname, './component/useChildComponent/useChildComponent.tsx'), 'component');
+      resolve();
+    })
   });
-  it('编译带子组件的jsx组件', async function() {
-    // const { output } = await compileOneComponent(path.resolve(__dirname, './component/useChildComponent-1/useChildComponent.jsx'));
-    await compile(path.resolve(__dirname, './component/useChildComponent-1/useChildComponent.jsx'), 'component');
+  it('编译带子组件的jsx组件', function() {
+    return new Promise(async resolve => {
+      await compile(path.resolve(__dirname, './component/useChildComponent-1/useChildComponent.jsx'), 'component');
+      resolve();
+    })
   });
   xit('可以给子组件传递prop')
   xit('子组件可以响应prop的变化')
