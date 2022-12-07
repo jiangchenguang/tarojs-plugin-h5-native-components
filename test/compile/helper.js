a52bd4c6a1c74ca88fa99e8a3a81c73e21c7d717
jcg01423690 <jcg01423690@alibaba-inc.com>
2022-12-07 23:36:50 +0800

    移动目录

--- test/compile/helper.js
+++ test/compile/helper.js
@@ -1,10 +1,12 @@
 import path from 'path';
 
-export const absCompPath = (compPath) => path.resolve(__dirname, compPath);
+export const absCompPath = (compPath) => {
+  return path.resolve(__dirname, compPath);
+}
 export const outputPath = compPath => {
   const idx = compPath.lastIndexOf('/');
   if (idx > -1) {
-    return path.resolve('dist/es', compPath.slice(0, idx));
+    return path.resolve('test-output/dist/es', compPath.slice(0, idx));
   } else {
     return 'dist/es'
   }
