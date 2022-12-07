997bab0309d46098ac2b9e68b47d3d42c7813170
jcg01423690 <jcg01423690@alibaba-inc.com>
2022-12-07 23:38:04 +0800

    解决文件默认文件扩展名的问题
    从package.json中判断

--- src/taro.js
+++ src/taro.js
@@ -1,12 +1,31 @@
 import { readConfig } from "@tarojs/helper";
 import path from 'path';
+import fs from 'fs';
+
+export const isUseTs = (packageJson) => {
+  const templateInfo = JSON.parse(packageJson).templateInfo;
+  if (templateInfo && 'typescript' in templateInfo) {
+    return templateInfo['typescript'];
+  } else {
+    throw new Error('package.json中未找到templateInfo，所以不能判断判断文件扩展名是tsx或jsx')
+  }
+};
 
 // 配置文件中的组件路径转换成绝对路径
-export const configPathToAbsPath = (configPath) => {
-  // taro配置的组件路径是忽略src的，且没有扩展类型
-  // todo taro组件路径是不带扩展名的，但是rollup的input是需要的，如何处理呢？
+export const configPathToAbsPath = (componentConfig) => {
+  // taro配置的组件路径是忽略src的，且没有扩展类型，例如：
   // [ 'components/Button/index', 'components/KeyAreas/index' ]
-  return path.resolve(process.cwd(), 'src/', configPath);
+  let fileName;
+  let filePath = '';
+  const fileExt = isUseTs(fs.readFileSync(path.resolve(process.cwd(), 'package.json'), 'utf8')) ? '.tsx' : '.jsx';
+  const idx = componentConfig.lastIndexOf('/');
+  if (idx > -1) {
+    fileName = componentConfig.slice(idx + 1) + fileExt;
+    filePath = componentConfig.slice(0, idx);
+  } else {
+    fileName = componentConfig + fileExt;
+  }
+  return path.resolve(process.cwd(), 'src/', filePath, fileName);
 }
 
 // 获取组件的所有祖先目录
